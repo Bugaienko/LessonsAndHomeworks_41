befa42f385825cafc217d565224cb2559251331a
Bugaienko <sergiibugaienko@gmail.com>
2024-01-19 21:41:01 +0100

    lesson_04 add

---
+++ src/homework_02/HomeWork_02.java
@@ -0,0 +1,15 @@
+package homework_02;
+/*
+@date 18.01.2024
+@author Sergey Bugaienko
+*/
+
+public class HomeWork_02 {
+
+//    Ctrl + A -> Ctrl + C -> в Слаке Ctrl + V - вставили текст и отправили
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+//        #  <  >  +  - ,  .  :  ; (  ) {  }  /  \
+        System.out.println("#  <  >  +  - ,  .  :  ; (  ) {  }  /   \\");
+    }
+}
